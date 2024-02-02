66d3e5aed6583325df5fddf008b0d77c24ad89de
Yakein Poonia <poonias5886@gmail.com>
2024-02-03 00:27:45 +0530

    added questions

--- DAY01/bitwiseoperator.cpp
+++ DAY01/bitwiseoperator.cpp
@@ -3,20 +3,29 @@ using namespace std;
 
 int main(){
 
-    int a;
-    int b;
+    int a=4;
+    int b=6;
 
-    cout<<"enter your first number:";
-    cin>>a;
-    cout<<"enter your second number:";
-    cin>>b;
-
+    // These are the examples of AND,OR,NOT and XOR 
     cout<<" a&b "<< (a&b)<<endl;
     cout<<" a|b "<< (a|b)<<endl;
     cout<<" a^b "<< (a^b)<<endl;
     cout<<" ~a "<< (~a)<<endl;
     cout<<" ~b "<< (~b)<<endl;
-    
+
+
+    // These are the examples of left and right shifts
+    cout<< (17>>1) <<endl;
+    cout<< (17>>2) <<endl;
+    cout<< (19<<1) <<endl;
+    cout<< (21<<2) <<endl;
+
+
+    // Pre-increatment and Post-increatment
+    cout<< (a++) << endl;
+    cout<< (++a) << endl;
+    cout<< (a--) << endl;
+    cout<< (--a) << endl;
 
     return 0;
 }
