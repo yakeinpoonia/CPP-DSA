29808cb1607deb2d0a742a007c2ff813fa5ba30c
Yakein Poonia <poonias5886@gmail.com>
2024-02-03 22:10:16 +0530

    added day02

---
+++ DAY02/forloop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cin>>n;
+    
+    int i=1;
+    for (;;)
+    {
+        if (i<=n)
+        {
+            cout<<i<<endl;
+        }
+        
+        i++;
+    }
+    
+
+    return 0;
+}
